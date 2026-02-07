c465732fa77dd5ec98f6749dc53576c105ba1866
xiorcale <17534323+xiorcale@users.noreply.github.com>
2026-02-07 18:33:39 +0100

    improve scrolling again

--- player/src/screen/player.rs
+++ player/src/screen/player.rs
@@ -3,28 +3,36 @@ use iced::{
     Length::Fill,
     Task,
     widget::{
-        Scrollable, Space, button, canvas, column, operation, row,
+        Id, Scrollable, Space, button, canvas, column,
+        operation::{self, AbsoluteOffset},
+        row,
         scrollable::{Direction, Scrollbar, Viewport},
     },
 };
 
-use crate::widget::{PianoRollRenderer, TrackRenderer};
+use crate::widget::{self, PianoRollRenderer, TrackRenderer};
 
 const PIANO_ROLL_ID: &str = "piano_roll";
+const TRACK_RENDERER_ID: &str = "track_renderer";
 
 pub struct Player {
     midi_file: Option<midi::File>,
+    track_hpos: f32,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     OpenFile(String),
-    SyncPianoRoll(Viewport),
+    PianoRollScroll(Viewport),
+    TrackScroll(Viewport),
 }
 
 impl Player {
     pub fn new() -> Self {
-        Player { midi_file: None }
+        Player {
+            midi_file: None,
+            track_hpos: 0.0,
+        }
     }
 
     pub fn update(&mut self, message: Message) -> Task<Message> {
@@ -33,7 +41,15 @@ impl Player {
                 self.midi_file = Some(midi::File::new(filepath));
                 Task::none()
             }
-            Message::SyncPianoRoll(viewport) => {
+            Message::PianoRollScroll(viewport) => operation::scroll_to(
+                TRACK_RENDERER_ID,
+                AbsoluteOffset {
+                    x: self.track_hpos,
+                    y: viewport.absolute_offset().y,
+                },
+            ),
+            Message::TrackScroll(viewport) => {
+                self.track_hpos = viewport.absolute_offset().x;
                 operation::scroll_to(PIANO_ROLL_ID, viewport.absolute_offset())
             }
         }
@@ -45,15 +61,17 @@ impl Player {
             let track_renderer = TrackRenderer::new(&midi_file.tracks[1]);
 
             let canvas_header = Scrollable::new(canvas(piano_roll_renderer).width(50).height(3500))
+                .id(PIANO_ROLL_ID)
                 .direction(Direction::Vertical(Scrollbar::hidden()))
-                .id(PIANO_ROLL_ID);
+                .on_scroll(Message::PianoRollScroll);
 
             let track_canvas = Scrollable::new(canvas(track_renderer).width(3000).height(3500))
+                .id(TRACK_RENDERER_ID)
                 .direction(Direction::Both {
                     vertical: Scrollbar::new(),
                     horizontal: Scrollbar::new(),
                 })
-                .on_scroll(Message::SyncPianoRoll);
+                .on_scroll(Message::TrackScroll);
 
             row![canvas_header, track_canvas].into()
         } else {
